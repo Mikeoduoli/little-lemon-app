f839a95508333e6fd876f06bcc806a2e0ac8b0de
Mikeoduoli <mikeoduoli@gmail.com>
2024-06-27 08:11:26 +0300

    Components for Little Lemon

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import './App.css';
 import Homepage from './components/Homepage';
 import Nav from './components/Nav';
 import Menu from './components/Menu';
+import Main from './components/Main';
+import Booking from './components/Booking';
+import BookingForm from './components/BookingForm';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
       < Nav />
       < Homepage />
+      < Main />
       < Menu />
+      < Booking />
+      < BookingForm />
+      < Footer />
     </>
   );
 }
