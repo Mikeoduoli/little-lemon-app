9e7e830fe1eb37173c8858f5243817fd99109bd3
Mikeoduoli <mikeoduoli@gmail.com>
2024-07-02 13:43:25 +0300

    Menu Added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Nav from './components/Nav';
 import Main from './components/Main';
+import Menu from './components/Menu';
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <>
       < Nav />
       < Main />
+      < Menu />
     </>
   );
 }
